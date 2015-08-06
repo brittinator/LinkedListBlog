a39b970ef0945439fcce25ba55722ae1cc3b0c88
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-06 11:09:33 -0700

    comment recursion methods.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,17 +6,31 @@ class Comment < ActiveRecord::Base
     Comment.find_by(id: next_id)
   end
 
-  def append(comment)
+  def append(comment) # recursive
     # places comment at the end of the list
+    if self.next.nil?
+      self.update!(next_id: comment.id)
+    else
+      self.next.append(comment) # recursion
+    end
   end
 
   def remove(id)
-
+    return if self.next.nil?
+    # edge case - if want to delete the 1st comment
+    # 1 - 4 - 6(rmv) - 10
+    if self.next.id == id
+      # self is 4
+      self.update!(next_id: self.next.next_id)
+      # currently 6 = now becomes 10
+    else
+      self.next.remove(id)
+    end
   end
 
   def to_ary # regular recursive
     # want as a list, this is called a traversal
-    if !self.next
+    if self.next.nil?
       [self] # base case
     else
       [self] + self.next.to_ary
