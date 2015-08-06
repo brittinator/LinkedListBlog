1259da97b307015845e082918f932a13356717e2
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-06 10:46:33 -0700

    recursive to_ary list.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,18 +1,27 @@
 class Comment < ActiveRecord::Base
 
   def next
+    # instance method, so you'll be calling this on a particular comment instance
+    # gets next comment from current comment and returns it
+    Comment.find_by(id: next_id)
   end
 
   def append(comment)
-
+    # places comment at the end of the list
   end
 
   def remove(id)
 
   end
 
-  def to_ary
-
+  def to_ary # regular recursive
+    # want as a list, this is called a traversal
+    if !self.next
+      [self] # base case
+    else
+      [self] + self.next.to_ary
+      # + is concatenation so [4] + [6] will be [4, 6]
+    end
   end
 end
 
